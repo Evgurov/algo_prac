9987a3e46061a0d767e64d7caa3314386d418456
Evgurov <evgurovv@yandex.ru>
2023-04-07 01:56:51 +0300

    comments added

--- sprint_5/final_problems/A.cpp
+++ sprint_5/final_problems/A.cpp
@@ -1,3 +1,24 @@
+// ID: 85285717
+// ==============================================================================
+// `heap_sort` function creates a heap for sorting. It adds all elements of an
+// array to heap and then on every step gets the smallest one, forming new sorted
+// array.
+// ------------------------------------------------------------------------------
+// Class `Heap` is implemented as min-heap structure with the smallest element on
+// top. It contains the following methods:
+// * void add(T elem) -- adds new element to the heap via siftup procedure
+//   > time complexity: O(log n)
+// * void remove(T elem) -- deletes elem from heap if there is one
+//   > time complexity: O(log n)
+// * T get_mini() -- return the minimal element in the heap (top one) 
+//   > time complexity: O(1)
+// ------------------------------------------------------------------------------
+// Time complexity: n * O(log n) ~ O(n log n)
+// Memory complexity: O(n)
+// ==============================================================================
+
+
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -6,12 +27,9 @@ std::ifstream in;
 std::ofstream out;
 
 struct Participant {
-    const std::string login;
-    const unsigned problems_solved;
-    const unsigned penalty;
-
-    Participant(std::string& login, unsigned problems_solved, unsigned penalty) : 
-        login(login), problems_solved(problems_solved), penalty(penalty) {} ;
+    std::string login;
+    unsigned problems_solved;
+    unsigned penalty;
 
     bool operator < (const Participant& second) const ;
     bool operator == (const Participant& second) const {return login == second.login;} ;
@@ -44,7 +62,7 @@ public:
     Heap() : heap(1), heap_size() {};
     void add(const T& elem);
     void remove(const T& elem);
-    T get_min() {return heap[1]};
+    T get_min() {return heap[1];};
 };
 
 template <class T>
@@ -71,35 +89,52 @@ void Heap<T>::remove(const T& elem) {
     if (idx <= heap_size) {
         heap[idx] = heap[heap_size];
         heap.pop_back();
+        heap_size--;
 
         while(idx <= heap_size) {
-        if (idx * 2 < heap_size) {
-            if (idx * 2 + 1 < heap.size()) {
-                int max_idx = get_max_idx(heap, idx * 2, idx * 2 + 1);
-                if (heap[idx] < heap[max_idx]) {
-                    std::swap(heap[idx], heap[max_idx]);
-                    idx = max_idx;
+            if (idx * 2 <= heap_size) {
+                if (idx * 2 + 1 <= heap_size) {
+                    int min_idx;
+                    if (heap[idx * 2] < heap[idx * 2 + 1]) {
+                        min_idx = idx * 2;
+                    } else {
+                        min_idx = idx * 2 + 1;
+                    }
+                    if (heap[min_idx] < heap[idx]) {
+                        std::swap(heap[idx], heap[min_idx]);
+                        idx = min_idx;
+                    } else {
+                        break;
+                    }
                 } else {
-                    break;
+                    if (heap[idx * 2] < heap[idx]) {
+                        std::swap(heap[idx], heap[idx * 2]);
+                        idx = idx * 2;
+                    } else {
+                        break;
+                    }
                 }
             } else {
-                if (heap[idx] < heap[idx * 2]) {
-                    std::swap(heap[idx], heap[idx * 2]);
-                    idx = idx * 2;
-                } else {
-                    break;
-                }
+                break;
             }
-        } else {
-            break;
         }
-    }
     }
 }
 
-template <class Iterator>
-void heap_sort(Iterator begin, Iterator end) {
+template <class T>
+void heap_sort(std::vector<T>& arr) {
+    Heap<T> heap;
+    auto iter = arr.cbegin();
+    while (iter != arr.cend()) {
+        heap.add(*iter);
+        iter++;
+    }
 
+    for (auto& elem : arr) {
+        T min_elem = heap.get_min();
+        elem = min_elem;
+        heap.remove(min_elem);
+    }
 }
 
 int main(){
@@ -109,17 +144,15 @@ int main(){
     unsigned participants_number;
     in >> participants_number;
 
-    std::vector<Participant> participants;
-
-    std::string login;
-    unsigned problems_solved, penalty;
+    std::vector<Participant> participants(participants_number);
 
     for (Participant& participant : participants) {
-        in >> login >> problems_solved >> penalty;
-        participants.push_back(Participant(login, problems_solved, penalty));
+        in >> participant.login
+           >> participant.problems_solved
+           >> participant.penalty;
     }
 
-    heap_sort(participants.begin(), participants.end());
+    heap_sort(participants);
 
     for (unsigned i = participants.size(); i-- > 0; ) {
         out << participants[i].login << std::endl;
--- sprint_5/final_problems/B.cpp
+++ sprint_5/final_problems/B.cpp
@@ -1,3 +1,29 @@
+// ID: 85229290
+// ================================================================================
+// Function `remove(root, key)` works as follows: 
+// 1. Find parent of the node which should be deleted (`find_node_parent` function)
+//   * If there is no node with such `key` then do nothing.
+// 2. Figure out whether the node being deleted is left or right child of its 
+// parent. Then form a new subtree on the left or on the right side of the parent
+// without deleted node. (`make_new_subtree` function)
+//   * For this find the parent of the node which should replace the node being
+//   deleted. This is the rightmost node in the left subtree or the leftmost node
+//   in the right one. (`find_replacing_node_parent` function) 
+//   * Then replace the deleted node with replacing node.
+//     > If it is in the left subtree of deleted node then it is right child of its
+//     parent, and vice versa.
+// 3. If the node being deleted is the root then form a new subtree without this 
+// node and this will be the resulting tree.
+// --------------------------------------------------------------------------------
+// Time complexity: 
+// * `find_node_parent` ~ O(log n)
+// * `find_replacing_node_parent` ~ O(log n) => `make_new_subtree` ~ O(log n)
+// * Then overall time complexity for `remove` ~ O(log n) + O(log n) ~ O(log n)
+// Memory complexity: O(1)
+// ================================================================================
+
+
+
 #ifndef REMOTE_JUDGE
 struct Node {  
   int value;  
@@ -101,7 +127,7 @@ Node* make_new_subtree(Node* node_to_delete) {
 Node* remove(Node* root, int key) {
     Node* node_to_delete_parent = find_node_parent(root, root, key);
 
-    // there is no node with `value = key`
+    // if there is no node with `value = key`
     if (node_to_delete_parent == nullptr) { 
         return root;
     }
@@ -140,9 +166,9 @@ void test() {
     Node node6({&node5, nullptr, 10});
     Node node7({&node3, &node6, 5});
     Node* newHead = remove(&node7, 10);
-    //assert(newHead->value == 5);
-    //assert(newHead->right == &node5);
-    //assert(newHead->right->value == 8);
+    assert(newHead->value == 5);
+    assert(newHead->right == &node5);
+    assert(newHead->right->value == 8);
 }
 
 int main() {
